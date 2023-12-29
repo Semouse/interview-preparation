c28d7aed93989ff1c4c24afeeb960b8f76775207
Leanid Barok <tratos149@gmail.com>
2023-12-29 12:58:23 +0300

    Add blocking queue examples.

---
+++ src/com/examples/concurrency/queue/blocking/Consumer.java
@@ -0,0 +1,25 @@
+package com.examples.concurrency.queue.blocking;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Consumer implements Runnable {
+
+    private final BlockingQueue<Integer> queue;
+
+    public Consumer(BlockingQueue<Integer> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Integer counter = queue.take();
+                System.out.println("Taking counter: " + counter + " from a queue...");
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
