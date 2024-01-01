a7e556f4c6f08bf4c8fb2cccb9fef3ed3e446afd
Leanid Barok <tratos149@gmail.com>
2024-01-01 10:22:19 +0300

    Add exchanger example.

---
+++ src/com/examples/concurrency/exchanger/Second.java
@@ -0,0 +1,28 @@
+package com.examples.concurrency.exchanger;
+
+import java.util.concurrent.Exchanger;
+
+public class Second implements Runnable {
+
+    private int counter;
+    private final Exchanger<Integer> exchanger;
+
+    public Second(Exchanger<Integer> exchanger) {
+        this.exchanger = exchanger;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            counter--;
+            System.out.println("First decremented the counter: " + counter);
+
+            try {
+                counter = exchanger.exchange(counter);
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
